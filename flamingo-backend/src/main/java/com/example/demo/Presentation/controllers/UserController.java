48ca1cece364ea8dbab18a308c92012751398a33
5miiss <abdullahkhames96@gmail.com>
2023-03-05 23:09:36 +0200

    disabled cors proplem by adding web mvc bean

--- flamingo-backend/src/main/java/com/example/demo/Presentation/controllers/UserController.java
+++ flamingo-backend/src/main/java/com/example/demo/Presentation/controllers/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin("*") 
+
 public class UserController {
 
     @Autowired
