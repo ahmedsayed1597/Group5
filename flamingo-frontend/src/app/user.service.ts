682c2e39d28a2ea39254aad0e7343f70d84ea6a8
5miiss <abdullahkhames96@gmail.com>
2023-03-05 23:10:24 +0200

    rempved cors

--- flamingo-frontend/src/app/user.service.ts
+++ flamingo-frontend/src/app/user.service.ts
@@ -10,7 +10,7 @@ export class UserService {
   constructor(private _HttpClient: HttpClient) { }
 
   signUp(userData: any): Observable<any> {
-    return this._HttpClient.post('https://api.storerestapi.com/auth/register', userData,{headers: {'Content-Type':'application/json; charset=UTF-8'}});
+    return this._HttpClient.post('http://localhost:9090/flamingo/users', userData,{headers: {'Content-Type':'application/json; charset=UTF-8'}});
   }
 
   signIn(userData: any): Observable<any> {
