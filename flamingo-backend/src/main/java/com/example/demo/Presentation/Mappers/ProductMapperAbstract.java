03842fd567d64c87924eae536b538c726bd171fa
5miiss <abdullahkhames96@gmail.com>
2023-03-08 07:40:25 +0200

    fixed product mapper bug

--- flamingo-backend/src/main/java/com/example/demo/Presentation/Mappers/ProductMapperAbstract.java
+++ flamingo-backend/src/main/java/com/example/demo/Presentation/Mappers/ProductMapperAbstract.java
@@ -1,32 +1,37 @@
 package com.example.demo.presentation.mappers;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.example.demo.buisness.dtos.requestDTOs.ProductRequestDto;
 import com.example.demo.buisness.services.interfaces.CategoriesService;
+import com.example.demo.repository.entities.Category;
 import com.example.demo.repository.entities.Product;
 
-@Mapper
-public abstract class ProductMapperAbstract{
+@Mapper(componentModel = "spring")
+public abstract class ProductMapperAbstract {
     
     @Autowired
     private CategoriesService categoriesService;
 
-    @Autowired 
-    private ProductMapper productMapper;
 
-    public ProductRequestDto fromEntityToDto(Product product){
-        ProductRequestDto dto = productMapper.fromEntityToDto(product);
-        dto.setCategoryid(product.getCategory().getId());
-        return dto;
+    @Mapping(source = "category",target = "categoryid" , qualifiedByName = "getID")
+    public abstract ProductRequestDto fromEntityToDto(Product Product);
 
+    @Mapping(source = "categoryid",target = "category" , qualifiedByName = "getCategory")
+    public abstract Product fromDtoToEntity(ProductRequestDto Product);
+
+
+    @Named("getID")
+    public int getID(Category category){
+        return category.getId();
     }
-    public Product fromDtoToEntity(ProductRequestDto dto){
-        Product product = productMapper.fromDtoToEntity(dto);
-        product.setCategory(categoriesService.getByID(dto.getCategoryid()).get());
-        
-        return product;
+
+    @Named("getCategory")
+    public Category getByID(int dto){
+        return categoriesService.getByID(dto).get();
 
     }
  
