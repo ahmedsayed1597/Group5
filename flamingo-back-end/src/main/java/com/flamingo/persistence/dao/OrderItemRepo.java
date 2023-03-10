3fb3ca96c18b075573c43695dcebdd75b75985dc
mahmoud <mahmoudadam24697>
2023-03-10 23:26:07 +0200

    cart section

---
+++ flamingo-back-end/src/main/java/com/flamingo/persistence/dao/OrderItemRepo.java
@@ -0,0 +1,9 @@
+package com.flamingo.persistence.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.flamingo.persistence.entities.OrderItem;
+
+public interface OrderItemRepo extends JpaRepository<OrderItem, Long>{
+    
+}
