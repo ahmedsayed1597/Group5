b3ac198d53b5f9ea2e3173a98a7c788b72fa2d33
5miiss <abdullahkhames96@gmail.com>
2023-03-11 14:35:10 +0200

    ProductDTO

--- flamingo-back-end/src/main/java/com/flamingo/presentation/dto/ProductDto.java
+++ flamingo-back-end/src/main/java/com/flamingo/presentation/dto/ProductDto.java
@@ -5,14 +5,15 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 public class ProductDTO {
+
     private Long productId;
 	private String productName;
 	private String image;
 	private String description;
 	private Integer quantity;
 	private double price;
-
+    
 }
