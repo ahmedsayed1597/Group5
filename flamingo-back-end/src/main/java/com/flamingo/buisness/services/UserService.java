9994144e6f9cd72ccea003f032c33a04b4c8f5cf
Mohamed-ELgazzar <mhmd.atef.elgazzar@gmail.com>
2023-03-12 14:51:10 +0200

    changes to user

--- flamingo-back-end/src/main/java/com/flamingo/buisness/services/UserService.java
+++ flamingo-back-end/src/main/java/com/flamingo/buisness/services/UserService.java
@@ -1,15 +1,16 @@
 package com.flamingo.buisness.services;
 
 import com.flamingo.presentation.dto.UserDTO;
+import com.flamingo.presentation.dto.UserResponse;
 
 public interface UserService {
 	UserDTO registerUser(UserDTO userDTO);
 	
-	// UserResponse getAllUsers(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
+	UserResponse getAllUsers(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
 	
-	// UserDTO getUserById(Long userId);
+	UserDTO getUserById(Long userId);
 	
-	// UserDTO updateUser(Long userId, UserDTO userDTO);
+	UserDTO updateUser(Long userId, UserDTO userDTO);
 	
-	// String deleteUser(Long userId);
+	String deleteUser(Long userId);
 }
--- flamingo-back-end/src/main/java/com/flamingo/buisness/services/UserServiceImpl.java
+++ flamingo-back-end/src/main/java/com/flamingo/buisness/services/UserServiceImpl.java
@@ -1,23 +1,33 @@
 package com.flamingo.buisness.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import com.flamingo.exception.APIException;
+import com.flamingo.exception.ResourceNotFoundException;
 import com.flamingo.persistence.dao.AddressRepo;
 import com.flamingo.persistence.dao.RoleRepo;
 import com.flamingo.persistence.dao.UserRepo;
 import com.flamingo.persistence.entities.Address;
 import com.flamingo.persistence.entities.Cart;
+import com.flamingo.persistence.entities.CartItem;
 import com.flamingo.persistence.entities.Role;
 import com.flamingo.persistence.entities.User;
 import com.flamingo.presentation.dto.AddressDTO;
+import com.flamingo.presentation.dto.CartDTO;
+import com.flamingo.presentation.dto.ProductDTO;
 import com.flamingo.presentation.dto.UserDTO;
+import com.flamingo.presentation.dto.UserResponse;
 
 import jakarta.transaction.Transactional;
 
@@ -36,8 +46,8 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private AddressRepo addressRepo;
 
-    @Autowired
-    private CartService cartService;
+    // @Autowired
+    // private CartService cartService;
 
     @Autowired
     private PasswordEncoder passwordEncoder;
@@ -90,34 +100,140 @@ public class UserServiceImpl implements UserService {
 
     }
 
-    // @Override
-    // public com.flamingo.buisness.services.UserDTO registerUser(com.flamingo.buisness.services.UserDTO userDTO) {
-    //     // TODO Auto-generated method stub
-    //     throw new UnsupportedOperationException("Unimplemented method 'registerUser'");
-    // }
+    @Override
+	public UserResponse getAllUsers(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder) {
+		Sort sortByAndOrder = sortOrder.equalsIgnoreCase("asc") ? Sort.by(sortBy).ascending()
+				: Sort.by(sortBy).descending();
 
-    // @Override
-    // public UserResponse getAllUsers(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder) {
-    //     // TODO Auto-generated method stub
-    //     throw new UnsupportedOperationException("Unimplemented method 'getAllUsers'");
-    // }
+		Pageable pageDetails = PageRequest.of(pageNumber, pageSize, sortByAndOrder);
+		
+		Page<User> pageUsers = userRepo.findAll(pageDetails);
+		
+		List<User> users = pageUsers.getContent();
 
-    // @Override
-    // public com.flamingo.buisness.services.UserDTO getUserById(Long userId) {
-    //     // TODO Auto-generated method stub
-    //     throw new UnsupportedOperationException("Unimplemented method 'getUserById'");
-    // }
+		if (users.size() == 0) {
+			throw new APIException("No User exists !!!");
+		}
 
-    // @Override
-    // public com.flamingo.buisness.services.UserDTO updateUser(Long userId,
-    //         com.flamingo.buisness.services.UserDTO userDTO) {
-    //     // TODO Auto-generated method stub
-    //     throw new UnsupportedOperationException("Unimplemented method 'updateUser'");
-    // }
+		List<UserDTO> userDTOs = users.stream().map(user -> {
+			UserDTO dto = modelMapper.map(user, UserDTO.class);
 
-    // @Override
-    // public String deleteUser(Long userId) {
-    //     // TODO Auto-generated method stub
-    //     throw new UnsupportedOperationException("Unimplemented method 'deleteUser'");
-    // }
+			if (user.getAddresses().size() != 0) {
+				dto.setAddress(modelMapper.map(user.getAddresses().stream().findFirst().get(), AddressDTO.class));
+			}
+
+			CartDTO cart = modelMapper.map(user.getCart(), CartDTO.class);
+
+			List<ProductDTO> products = user.getCart().getCartItems().stream()
+					.map(item -> modelMapper.map(item.getProduct(), ProductDTO.class)).collect(Collectors.toList());
+
+			dto.setCart(cart);
+
+			dto.getCart().setProducts(products);
+
+			return dto;
+
+		}).collect(Collectors.toList());
+
+		UserResponse userResponse = new UserResponse();
+		
+		userResponse.setContent(userDTOs);
+		userResponse.setPageNumber(pageUsers.getNumber());
+		userResponse.setPageSize(pageUsers.getSize());
+		userResponse.setTotalElements(pageUsers.getTotalElements());
+		userResponse.setTotalPages(pageUsers.getTotalPages());
+		userResponse.setLastPage(pageUsers.isLast());
+		
+		return userResponse;
+	}
+
+	@Override
+	public UserDTO getUserById(Long userId) {
+		User user = userRepo.findById(userId)
+				.orElseThrow(() -> new ResourceNotFoundException("User", "userId", userId));
+
+		UserDTO userDTO = modelMapper.map(user, UserDTO.class);
+
+		userDTO.setAddress(modelMapper.map(user.getAddresses().stream().findFirst().get(), AddressDTO.class));
+
+		CartDTO cart = modelMapper.map(user.getCart(), CartDTO.class);
+
+		List<ProductDTO> products = user.getCart().getCartItems().stream()
+				.map(item -> modelMapper.map(item.getProduct(), ProductDTO.class)).collect(Collectors.toList());
+
+		userDTO.setCart(cart);
+
+		userDTO.getCart().setProducts(products);
+
+		return userDTO;
+	}
+
+	@Override
+	public UserDTO updateUser(Long userId, UserDTO userDTO) {
+		User user = userRepo.findById(userId)
+				.orElseThrow(() -> new ResourceNotFoundException("User", "userId", userId));
+
+		String encodedPass = passwordEncoder.encode(userDTO.getPassword());
+
+		user.setFirstName(userDTO.getFirstName());
+		user.setLastName(userDTO.getLastName());
+		user.setMobileNumber(userDTO.getMobileNumber());
+		user.setEmail(userDTO.getEmail());
+		user.setPassword(encodedPass);
+
+		if (userDTO.getAddress() != null) {
+			String country = userDTO.getAddress().getCountry();
+			String state = userDTO.getAddress().getState();
+			String city = userDTO.getAddress().getCity();
+			String pincode = userDTO.getAddress().getPincode();
+			String street = userDTO.getAddress().getStreet();
+			String buildingName = userDTO.getAddress().getBuildingName();
+
+			Address address = addressRepo.findByCountryAndStateAndCityAndPincodeAndStreetAndBuildingName(country, state,
+					city, pincode, street, buildingName);
+
+			if (address == null) {
+				address = new Address(country, state, city, pincode, street, buildingName);
+
+				address = addressRepo.save(address);
+
+				user.setAddresses(List.of(address));
+			}
+		}
+
+		userDTO = modelMapper.map(user, UserDTO.class);
+
+		userDTO.setAddress(modelMapper.map(user.getAddresses().stream().findFirst().get(), AddressDTO.class));
+
+		CartDTO cart = modelMapper.map(user.getCart(), CartDTO.class);
+
+		List<ProductDTO> products = user.getCart().getCartItems().stream()
+				.map(item -> modelMapper.map(item.getProduct(), ProductDTO.class)).collect(Collectors.toList());
+
+		userDTO.setCart(cart);
+
+		userDTO.getCart().setProducts(products);
+
+		return userDTO;
+	}
+
+	@Override
+	public String deleteUser(Long userId) {
+		User user = userRepo.findById(userId)
+				.orElseThrow(() -> new ResourceNotFoundException("User", "userId", userId));
+
+		List<CartItem> cartItems = user.getCart().getCartItems();
+		Long cartId = user.getCart().getCartId();
+
+		cartItems.forEach(item -> {
+
+			Long productId = item.getProduct().getProductId();
+
+			// cartService.deleteProductFromCart(cartId, productId);
+		});
+
+		userRepo.delete(user);
+
+		return "User with userId " + userId + " deleted successfully!!!";
+	}
 }
